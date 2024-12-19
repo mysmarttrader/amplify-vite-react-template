7f8e51d5e0fd3e191b32523c548271fb14924243
Robert Grano <rjgrano@gmail.com>
2024-12-18 22:05:13 -0500

    login stuff

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,5 +7,28 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    federation: {
+      google: {
+        clientId: 'YOUR_GOOGLE_CLIENT_ID',
+      },
+      facebook: {
+        appId: 'YOUR_FACEBOOK_APP_ID',
+      },
+    },
+  },
+  signupAttributes: [
+    'email',
+    'name',
+    'preferred_username',
+    'phone_number',
+    'birthdate',
+    'custom:address',
+    'custom:gender',
+  ],
+  passwordPolicy: {
+    minLength: 8,
+    requireUppercase: true,
+    requireNumbers: true,
+    requireSymbols: false,
   },
 });
